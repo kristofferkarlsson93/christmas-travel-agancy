2f69878a863783643a51d45c33d7f977b250e94c
Kristoffer Karlsson <kristoffer.karlsson@solidar.se>
2017-12-11 13:42:14 +0100

    started to add js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+    initEvents();
+}
+
+const initEvents = () => {
+    const form = document.body.querySelector('.login-form');
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        const value = form.input.value.toLowerCase();
+        if (value === "rätt svar") {
+            document.body.querySelector('.login-container').style.display = 'none';
+            document.body.style.background = "none";
+            document.body.querySelector('.main-content').display = 'block';
+        } else {
+            form.input.classList.add('is-invalid');
+            form.input.style.backgroundColor = "#fc8585";
+        }
+        return false;
+    });
+}
