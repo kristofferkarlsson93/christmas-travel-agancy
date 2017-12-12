7342901e77455d8b532259ab230378f32324a768
Kristoffer Karlsson <kristoffer.karlsson@solidar.se>
2017-12-12 13:12:03 +0100

    Working

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const initEvents = () => {
         if (value === "rätt svar") {
             document.body.querySelector('.login-container').style.display = 'none';
             document.body.style.background = "none";
-            document.body.querySelector('.main-content').display = 'block';
+            document.body.querySelector('.main-content').style.display = 'block';
         } else {
             form.input.classList.add('is-invalid');
             form.input.style.backgroundColor = "#fc8585";
